630608329038fa72a3611c714b6db8ee20c4274c
Youngmin Byeon <byeonely@gmail.com>
2021-12-29 01:32:31 +0900

    - 주석문 추가 - run, with 구문 리턴값 테스트 추가

--- src/main/kotlin/play2.kt
+++ src/main/kotlin/play2.kt
@@ -7,10 +7,12 @@ fun main() {
     // 인스턴스 -> 스코프 함수
     // 코드의 가독성을 향상시킴
 
+    // 인스턴스 반환, 인스턴스 초기화할 때 사용
     // apply : 인스턴스를 생성 후 변수에 담기 전에 '초기화 과정' 을 수행하여 인스턴스 자신을 반환함
     //         참조연산자 없이 인스턴스의 변수와 함수를 사용
     // also : it을 통해서 인스턴스를 사용. 처리가 끝나면 인스턴스를 반환
 
+    // 마지막 구문값 반환, 인스턴스의 속성이나 함수를 스코프 내에서 깔끔하게 조작할 때 사용
     // run == with : 인스턴스가 이미 만들어진 후 변수에 담기 전에 '조작 과정' 을 수행한 뒤 마지막 구문의 값을 반환함
     //               참조연산자 없이 인스턴스의 변수와 함수를 사용
     // let : it을 통해서 인스턴스를 사용. 처리가 끝나면 최종값을 반환
@@ -24,13 +26,16 @@ fun main() {
 
     println("a apply 상품명: ${a.name}, ${a.price}원")
 
-    a.run {
+    val runResult = a.run {
         println("a.run 상품명: ${name}, 가격: ${price}원")
     }
+    println("run 수행 후 리턴값 : $runResult")
 
-    with(a) {
+    val withResult = with(a) {
         println("with(a) 상품명: ${name}, 가격: ${price}원")
+        2
     }
+    println("with 수행 후 리턴값 : $withResult")
 
     a.let {
         println("a.let 상품명: ${it.name}, 가격: ${it.price}원")
