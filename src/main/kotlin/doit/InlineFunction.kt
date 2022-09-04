1e944d9f24fcda94344e748bf15a5dc3c41ac8ef
byeonely <byeonely@gmail.com>
2022-09-05 00:32:18 +0900

    - 제목 및 함수 설명 주석문 추가

--- src/main/kotlin/doit/InlineFunction.kt
+++ src/main/kotlin/doit/InlineFunction.kt
@@ -1,5 +1,9 @@
 package doit
 
+// 인라인 함수
+// 함수가 호출되는 곳에 함수 본문의 내용을 모두 복사해 넣는 것을 수행함.
+// 이 같은 특성으로 함수의 분기 없이 처리되기 때문에 코드의 성능을 높일 수 있음.(overhead 가 없음)
+// 람다식 매개변수를 가지고 있는 함수에서 동작함.
 fun main() {
     shortFunc(3) { println("First call: $it") }
     shortFunc(5) { println("Second call: $it") }
