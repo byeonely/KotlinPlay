0e6662376148da33e5875ec6edd74671ca22c9b0
byeonely <byeonely@gmail.com>
2022-10-05 00:10:10 +0900

    - 람다식 예제 함수 실행 및 출력 구문 추가

--- src/main/kotlin/doit/HighOrderTest.kt
+++ src/main/kotlin/doit/HighOrderTest.kt
@@ -15,19 +15,23 @@ fun main() {
     result = multi2(20, 20)
     println(result)
 
-    // 다음은 모두 같은 표현임
+    // 다음은 모두 같은 람다식 표현임
     val multiLamda1: (Int, Int) -> Int = {x: Int, y: Int -> x * y} // 생략되지 않은 전체 표현
     val multiLamda2 = {x: Int, y: Int -> x * y} // 선언 자료형 생략
     val multiLamda3: (Int, Int) -> Int = {x, y -> x * y} // 람다식 매개변수 자료형의 생략
 
-    val greet: () -> Unit = { println("Hello World!") } // 반환 자료형과 매개변수가 모두 없는 경우
-    val greet2 = { println("Hello World!") } // 람다식의 자료형 선언 생략 가능
+    val greet: () -> Unit = { println("Hello World! 1") } // 반환 자료형과 매개변수가 모두 없는 경우
+    val greet2 = { println("Hello World! 2") } // 람다식의 자료형 선언 생략 가능
     val square: (Int) -> Int = {x -> x * x} // 반환 자료형과 매개변수가 각각 하나인 경우
     val square2: () -> Int = { 2 } // 반환 자료형만 있고 매개변수는 없는 경우
+    greet()
+    greet2()
+    println(square(2))
     println(square2())
 
     // 람다식의 자료형 선언 생략
     val skip1 = { println("Hello World!") } // 추론 가능
     val skip2 = {x: Int -> x * x} // skip2 의 자료형을 생략하려면 x 의 자료형을 명시해야 함
     val skip3 = {{ println("nested") }} // 추론 가능
+    skip3().invoke()
 }
